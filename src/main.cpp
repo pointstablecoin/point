dfd04e8447a39ba64ac2e2c55b735c3cc0618b58
juan tellez <juantellez@gootechnology.com>
2019-03-26 17:07:29 -0600

    First

--- src/main.cpp
+++ src/main.cpp
@@ -1582,8 +1582,9 @@ int64_t GetBlockValue(int nHeight)
     LogPrintf("############################PRINT TEST MONEY SUPPLY: %d.\n", FormatMoney(nMoneySupplyActual));
     LogPrintf("############################PRINT TEST NUMERO DE TXS: %d.\n", nTx);
     
-    for (auto i: pindexActual)
+    for (auto i: pindexActual){}
         std::cout << i << ' ';
+}
     
     if (nHeight == 0) {
         return 100000000 * COIN;
