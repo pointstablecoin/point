cfc1afd2ac9d5636fa986448c63f35d3e6691157
juan tellez <juantellez@gootechnology.com>
2019-03-26 18:23:24 -0600

    First

--- src/main.h
+++ src/main.h
@@ -237,7 +237,7 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCou
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader* pblock, bool fProofOfStake);
 
 bool ActivateBestChain(CValidationState& state, CBlock* pblock = NULL, bool fAlreadyChecked = false);
-CAmount GetBlockValue(int nHeight);
+CAmount GetBlockValue(int nHeight, CAmount nValueOut);
 
 /** Create a new block index entry for a given block hash */
 CBlockIndex* InsertBlockIndex(uint256 hash);
