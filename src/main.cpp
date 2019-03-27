f3346958b3659bec2ba0c8d92e97773b98ca1a41
juan tellez <juantellez@gootechnology.com>
2019-03-27 03:33:19 -0600

    First

--- src/main.cpp
+++ src/main.cpp
@@ -1585,10 +1585,10 @@ int64_t GetBlockValue(int nHeight)
         
     double nVelocidad = nValorTotal / nMoneySupplyActual;
     
-    LogPrintf("############################PRINT TEST MONEY SUPPLY: %d.\n", nMoneySupplyActual);
-    LogPrintf("############################PRINT TEST NUMERO DE TXS: %d.\n", nTx);  
-    LogPrintf("############################PRINT TEST VALORTOTAL: %d.\n", nValorTotal);
-    LogPrintf("############################PRINT TEST VELOCIDAD: %d.\n", nVelocidad);
+    LogPrintf("############################PRINT TEST MONEY SUPPLY: %.8g\n", nMoneySupplyActual);
+    LogPrintf("############################PRINT TEST NUMERO DE TXS: %d\n", nTx);  
+    LogPrintf("############################PRINT TEST VALORTOTAL: %d\n", nValorTotal);
+    LogPrintf("############################PRINT TEST VELOCIDAD: %.8g\n", nVelocidad);
     
     LogPrintf("\n\n");
     
