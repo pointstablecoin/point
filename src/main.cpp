942da2c9dbfa5b5e85bb20c4ab4d24bda85569c3
juan tellez <juantellez@gootechnology.com>
2019-03-26 22:28:28 -0600

    First

--- src/main.cpp
+++ src/main.cpp
@@ -1582,7 +1582,9 @@ int64_t GetBlockValue(int nHeight)
     CAmount nMoneySupplyAnterior = pindexActual->pprev->nMoneySupply;
     
     LogPrintf("############################PRINT TEST MONEY SUPPLY: %d.\n", FormatMoney(nMoneySupplyActual));
-    LogPrintf("############################PRINT TEST NUMERO DE TXS: %d.\n", nTx);    
+    LogPrintf("############################PRINT TEST NUMERO DE TXS: %d.\n", nTx);  
+    LogPrintf("############################PRINT TEST VALORTOTAL: %d.\n", nValueOut);  
+    
     if (nHeight == 0) {
         return 100000000 * COIN;
     } else if (nHeight > 0){
