fd071f9e4bb35ef901a270c563e672b15329d3c0
juan tellez <juantellez@gootechnology.com>
2019-04-03 18:03:37 -0600

    Revert "Rebranding"
    This reverts commit a5992594f94bc040f9a9e7f62d6b131a1c3b7565.

--- src/obfuscation.cpp
+++ src/obfuscation.cpp
@@ -1343,7 +1343,7 @@ bool CObfuscationPool::DoAutomaticDenominating(bool fDryRun)
         // should have some additional amount for them
         nLowestDenom += OBFUSCATION_COLLATERAL * 4;
 
-    CAmount nBalanceNeedsAnonymized = nAnonymizePointAmount * COIN;
+    CAmount nBalanceNeedsAnonymized = nAnonymizeMergeAmount * COIN;
 
     // if balanceNeedsAnonymized is more than pool max, take the pool max
     if (nBalanceNeedsAnonymized > OBFUSCATION_POOL_MAX) nBalanceNeedsAnonymized = OBFUSCATION_POOL_MAX;
