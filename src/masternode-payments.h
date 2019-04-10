9016f70d0f8f1248ece37ebe6d1b5f3ca66f72e3
juan tellez <juantellez@gootechnology.com>
2019-04-10 03:45:09 -0500

    Modificaciones

--- src/masternode-payments.h
+++ src/masternode-payments.h
@@ -30,7 +30,7 @@ extern CMasternodePayments masternodePayments;
 void ProcessMessageMasternodePayments(CNode* pfrom, std::string& strCommand, CDataStream& vRecv);
 bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight);
 std::string GetRequiredPaymentsString(int nBlockHeight);
-bool IsBlockValueValid(const CBlock& block, CAmount nExpectedValue, CAmount nMinted CAmount nMasternodePayment);
+bool IsBlockValueValid(const CBlock& block, CAmount nExpectedValue, CAmount nMinted, CAmount nMasternodePayment);
 void FillBlockPayee(CMutableTransaction& txNew, CAmount nFees, bool fProofOfStake);
 
 void DumpMasternodePayments();
