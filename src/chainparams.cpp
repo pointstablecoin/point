6f91e80f30e69d6adba0b8a30013e171116df2b3
juan tellez <juantellez@gootechnology.com>
2019-05-06 23:07:14 -0500

    Revert "Posible fix"
    This reverts commit de75efaff42c2b9f7e2047be62770574e21ab72d.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -169,7 +169,7 @@ public:
         pchMessageStart[2] = 0xef;
         pchMessageStart[3] = 0x3f;
         vAlertPubKey = ParseHex("04e4947934c23ec1c2dc01daf414a107916d2f1db14cd198f87541b98512f7e62f696ddc2d4ff2674663ddbd1de6f64deea885b45c4280df8db18f459389174839");
-        nDefaultPort = 72100;
+        nDefaultPort = 62100;
         bnProofOfWorkLimit = ~uint256(0) >> 20;
         nMaxReorganizationDepth = 100;
         nEnforceBlockUpgradeMajority = 750;
